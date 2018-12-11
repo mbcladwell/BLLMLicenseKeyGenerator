dd2c874148c14f2e18d42e9857ddd3402e5780aa
mbcladwell <mbcladwell@stihie.net>
2018-12-11 09:30:04 -0500

    a commit prior to incorporating cryptography

---
+++ src/main/java/CryptoException.java
@@ -0,0 +1,11 @@
+package bllmlkg;
+
+public class CryptoException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+  public CryptoException() {}
+
+  public CryptoException(String message, Throwable throwable) {
+    super(message, throwable);
+  }
+}
